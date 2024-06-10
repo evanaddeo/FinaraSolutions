e01131a0134df295dc02517602c7a597313acd0b
Evan Addeo <evanaddeo@gmail.com>
2024-06-09 23:47:16 -0400

    Structured unsigned welcome page

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import GooglePlay from '../assets/images/GooglePlayLogo.png'
+import Apple from '../assets/images/AppleLogo.png'
 import { useNavigate } from 'react-router-dom'
-import { SIZES } from '../constants/themes'
+import { FONTS, SIZES, COLORS } from '../constants/themes'
 
 import '../styles/Welcome.css'
 
@@ -16,34 +19,105 @@ function Welcome() {
             <button onClick={ () => { navi('/Login') } }>Go to login</button>
             <button onClick={ () => { navi('/Signup') } }>Go to signup</button>
 
-            <p style={{fontSize: SIZES.xxsmall}}>xxsmall</p>
-            <p style={{fontSize: SIZES.xsmall}}>xsmall</p>
-            <p style={{fontSize: SIZES.small}}>small</p>
-            <p style={{fontSize: SIZES.medium}}>medium</p>
-            <p style={{fontSize: SIZES.large}}>large</p>
-            <p style={{fontSize: SIZES.xlarge}}>xlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
-            <p style={{fontSize: SIZES.xxlarge}}>xxlarge</p>
+            <p>Hello</p>
+            <p>Hello</p>
+
+            {/* Intro Hero Section*/}
+            <div className='sectionContainer'>
+
+                <div className='subsection' style={{alignItems: 'flex-start'}}>
+                    <p style={{fontSize: SIZES.xxxlarge, fontFamily: FONTS.Title, color: COLORS.black}}>
+                        The Modern <br></br>Budgeting Solution <br></br>with <span style={{color: COLORS.blue1}}>Finara</span>
+                    </p>
+                    <p className='secondaryTxt'>
+                        Record every transaction in seconds, analyze spend metrics, <br></br>
+                        and review categorized spending history to achieve all of your <br></br>
+                        financial goals and build a better future.
+                    </p>
+                    <button 
+                        className='actionBtn' 
+                        style={ {fontFamily: FONTS.Regular, fontSize: SIZES.small}}
+                        onClick={() => { navi('/Signup')}}>
+                        Get Started
+                    </button>
+                </div>
+
+                <div className='subsection'>
+                    <p>Photo & info</p>
+                </div>
+            </div>
+
+            {/* Product Features Section*/}
+            <div className='sectionContainer'>
+                <div className='subsection' style={{alignItems: 'flex-start'}}>
+                    <p style={{fontSize: SIZES.xxlarge, fontFamily: FONTS.Title, color: COLORS.black}}>Product Features</p>
+                    <p className='secondaryTxt'>
+                        Customize your budget plan, analyze spend metrics, and get a snapshot of your financial status from your Desktop or mobile device.
+                    </p>
+                    <p>Dashboard</p>
+                    <p>Advanced Metrics</p>
+                    <p>Budget Tools</p>
+                </div>
+                <div className='subsection'>
+                    <p>Graphic on selected Ft</p>
+                </div>
+            </div>
+
+            {/* Mobile App Section*/}
+            <div className='sectionContainer'>
+
+                <div className='subsection'>
+                    <p>large phone</p>
+                </div>
+
+                <div className='subsection' style={{alignItems: 'flex-end', textAlign: 'right'}}>
+                    <p style={{fontSize: SIZES.xxlarge, fontFamily: FONTS.Title, color: COLORS.black}}>The Best App to Support Your <br></br>Financial Future</p>
+                    <p className='secondaryTxt'>
+                        Don't let your on-the-go lifestyle get in the way of your budgeting <br></br>
+                        goals. Add & edit expenses anywhere, anytime. 
+                    </p>
+
+                    <div className='subsectionRow'>
+
+                        <button className='mobileDownload' style={{marginRight: '.5vw'}}>
+                            <div className='mobileContent'>
+                                <img className='mobileLogo' src={Apple} style={{width: '17px', marginTop: '-2px'}}alt='Apple Logo' />
+
+                                <div className='mobileSpecifics'>
+                                    <p className='txt' style={{fontSize: '.5rem'}}>Get on the</p>
+                                    <p className='txt' style={{fontSize: '.8rem'}}>App Store</p>
+                                </div>
+                            </div>
+                        </button>
+
+                        <button className='mobileDownload'>
+                            <div className='mobileContent'>
+                                <img className='mobileLogo' src={GooglePlay} alt='Play Logo' />
+
+                                <div className='mobileSpecifics'>
+                                    <p className='txt' style={{fontSize: '.5rem'}}>Get on the</p>
+                                    <p className='txt' style={{fontSize: '.8rem'}}>Play Store</p>
+                                </div>
+                            </div>
+                        </button>
+
+                    </div>
+                </div>              
+            </div>
+
+            {/* Get Started Section*/}
+            <div className='sectionContainer'>
+                <div className='subsection' style={{textAlign: 'center'}}>
+                    <p style={{fontSize: SIZES.xxlarge, fontFamily: FONTS.Title, color: COLORS.black}}>Let's Spend <span style={{color: COLORS.blue1}}>Smart</span>.</p>
+                    <p className='secondaryTxt'>Explore our cutting-edge financial health services, delivering high-quality solutions tailored to your needs. <br></br>Elevate your experience with top-tier features from Finara.</p>
+                    <div className='subsectionRow'>
+                        <p>Email entry btn</p>
+                        <p>____submit btn</p>
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
         </div>
     )
 }
